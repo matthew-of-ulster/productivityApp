a057f4fc078e159131fafc093bedb1966b13ce2c
callum-ferguson24 <callumferguson969@gmail.com>
2023-02-11 13:57:43 +0000

    Button Added

--- app/src/main/java/com/example/productivityapp/MainActivity.java
+++ app/src/main/java/com/example/productivityapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.productivityapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView firstText;
+        Button firstButton;
+
+        firstText = (TextView) findViewById(R.id.my_text_view);
+        firstButton = (Button) findViewById(R.id.button);
+
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstText.setText("Hi Matthew");
+            }
+        });
+
+
     }
 }
