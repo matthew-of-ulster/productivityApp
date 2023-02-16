2414ad6d038dff2b4e65afd038cb66115bb6309a
Matthew <matthewhwallace17@gmail.com>
2023-02-16 22:44:53 +0000

    Timer moved to fragment, scrollview,implemented

--- app/src/main/java/com/example/productivityapp/MainActivity.java
+++ app/src/main/java/com/example/productivityapp/MainActivity.java
@@ -1,16 +1,8 @@
 package com.example.productivityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.FragmentManager;
-
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,12 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        timer1Et = (EditText) findViewById(R.id.timerEditText);
-        textView = (TextView) findViewById(R.id.text1);
-        progress = (ProgressBar) findViewById(R.id.timerProgress1);
-        progress.setProgress(100);
-        //int counterMax = 5;
 
         FragmentManager fragmentManager = getSupportFragmentManager();
 
@@ -73,41 +59,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        progress.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int counterMax = 5;
-                String input = String.valueOf(timer1Et.getText());
 
-                if(!input.equals("")) {
-                    counterMax = Integer.parseInt(String.valueOf(timer1Et.getText()));
-                }
-                progress.setProgress(100);
-                counter = counterMax;
-                int finalCounterMax = counterMax;
-                new CountDownTimer(finalCounterMax*1000, 1000){
-
-                    @Override
-                    public void onTick(long millisUntilFinish) {
-                       textView.setText(String.valueOf(counter));
-                       progress.setProgress(counter*(100/ finalCounterMax));
-                       counter--;
-                    }
-
-                    @Override
-                    public void onFinish() {
-                        textView.setText("FINISH!!");
-                        createNotification(view);
-                        progress.setProgress(0);
-                    }
-                }.start();
-            }
-        });
-    }
-    public void createNotification(View view) {
-
-        Intent i = new Intent(this, NotificationBroadcast.class);
-        sendBroadcast(i);
     }
 }
+
 
--- app/src/main/java/com/example/productivityapp/TimerFragment.java
+++ app/src/main/java/com/example/productivityapp/TimerFragment.java
@@ -1,12 +1,24 @@
 package com.example.productivityapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import androidx.core.app.NotificationCompat;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.CountDownTimer;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +26,11 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class TimerFragment extends Fragment {
+
+    public int counter;
+    TextView textView;
+    EditText timer1Et;
+    ProgressBar progress;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -53,12 +70,65 @@ public class TimerFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer, container, false);
+        View view = inflater.inflate(R.layout.fragment_timer, container, false);
+        TextView middleText;
+        middleText= (TextView) view.findViewById(R.id.text1);
+        middleText.setText("Nice");
+
+        timer1Et = (EditText) view.findViewById(R.id.timerEditText);
+        textView = (TextView) view.findViewById(R.id.text1);
+        progress = (ProgressBar) view.findViewById(R.id.timerProgress1);
+        progress.setProgress(100);
+        //int counterMax = 5;
+
+        progress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int counterMax = 5;
+                String input = String.valueOf(timer1Et.getText());
+
+                if(!input.equals("")) {
+                    counterMax = Integer.parseInt(String.valueOf(timer1Et.getText()));
+                }
+                progress.setProgress(100);
+                counter = counterMax;
+                int finalCounterMax = counterMax;
+                new CountDownTimer(finalCounterMax*1000, 1000){
+
+                    @Override
+                    public void onTick(long millisUntilFinish) {
+                        textView.setText(String.valueOf(counter));
+                        progress.setProgress(counter*(100/ finalCounterMax));
+                        counter--;
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        textView.setText("FINISH!!");
+                        //createNotification(view);
+                        progress.setProgress(0);
+                    }
+                }.start();
+            }
+        });
+
+
+
+        return view;
     }
+    /*
+    public void createNotification(View view) {
+
+        Intent i = new Intent(this, NotificationBroadcast.class);
+        sendBroadcast(i);
+    }
+     */
 }
